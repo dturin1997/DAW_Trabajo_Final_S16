3e42055338aee5a0428f9c4f152a428bae66261d
Dario Turin <dturin1997@gmail.com>
2022-06-27 17:59:31 -0500

    Se eleminó archivos innecesarios!

--- src/main/java/com/web/repositorios/IVenta.java
+++
@@ -1,9 +0,0 @@
-package com.web.repositorios;
-import java.io.Serializable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.web.modelo.Venta;
-@Repository
-public interface IVenta extends JpaRepository<Venta, Serializable>{
-	public abstract Venta findByCodigo(Long codigo);
-}
